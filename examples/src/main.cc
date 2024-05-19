b0c9267020f0687af2f41847901aa3fd55e751a9
GordonZhang2024 <jp20171211@163.com>
2024-05-19 00:49:19 +0000

    Update the example

--- examples/src/main.cc
+++ examples/src/main.cc
@@ -1,17 +1,26 @@
 #include "log.h"
 
+/*
+ * This is the example program.
+ */
+
 int main()
 {
 	logger l("log.txt");
-	l.log(ERROR, "error message");
+	
+    l.log(ERROR, "error message");
 	l.log(DEBUG, "debug message");
 	
+    // Disable the logger
 	l.disable();
-	l.log(DEBUG, "This message will never be shown");
+	l.log(DEBUG, "This message will never be shown."); // This message will never be shown.
 	
+    // Enable the logger again
 	l.enable();
+    // Set the log level to warning.
+    // Now only the messages whose level >= WARNING can be shown.
 	l.set_log_level(WARNING);
-	l.log(INFO, "This message will never be shown");
+	l.log(INFO, "This message will never be shown."); // This message will never be shown because Its level is too low.
 
 	return 0;
 }
