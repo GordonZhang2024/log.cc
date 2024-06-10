bc504123eb60bc97061b08d1bf28b714a45460a6
Gordon Zhang <jp20171211@163.com>
2024-06-10 18:20:45 +0800

    Add the copyright notice and update the example.

--- examples/src/main.cc
+++ examples/src/main.cc
@@ -1,4 +1,29 @@
+/* log.cc | A simple logging library for C++
+ * ========================================
+ * License : MIT License
+ * Copyright (c) 2024 Gordon Zhang
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+  */
+
 #include "log.h"
+#include <iostream>
 
 /*
  * This is the example program.
@@ -7,6 +32,9 @@
 int main()
 {
 	logger l("log.txt");
+
+	std::cout << "This is the example program for log.cc." << std::endl;
+	std::cout << std::endl;
 	
 	l.log(ERROR, "error message");
 	l.log(DEBUG, "debug message");
